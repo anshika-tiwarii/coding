2920d104988d7302d17135c88cd4d6500711af29
Anshika Tiwari <anshikatiwari2255@gmail.com>
2025-07-21 13:28:46 +0530

    WIP: temporary changes before pulling

--- cpp_codes/practicee.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-#include<iostream>
-using namespace std;
-int main(){
-    string word;
-    cout<<"please enter a word:  ";
-    getline(cin,word);
-    cout<<"the word is:  "<<word;
-}
