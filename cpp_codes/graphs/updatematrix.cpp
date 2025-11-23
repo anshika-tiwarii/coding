d603644be299378bc61d8904b2290c9a898773ef
Anshika Tiwari <anshikatiwari2255@gmail.com>
2025-11-23 11:40:07 +0530

    find distance of nearest 0

---
+++ cpp_codes/graphs/updatematrix.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> vis(n,vector<int> (m,0));
+        vector<vector<int>> dist(n,vector<int> (m,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    vis[i][j]=1;
+                    q.push({{i,j},0});
+                }
+            }
+        }
+            int delRow[]={-1,0,+1,0};
+            int delCol[]={0,+1,0,-1};
+            while(!q.empty()){
+                int r=q.front().first.first;
+                int c=q.front().first.second;
+                int d=q.front().second;
+                q.pop();
+                dist[r][c]=d;
+                for(int i=0;i<4;i++){
+                    int nrow=r+delRow[i];
+                    int ncol=c+delCol[i];
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
+                        vis[nrow][ncol]=1;
+                        q.push({{nrow,ncol},d+1});
+                    }
+                }
+            }
+            return dist;
+    }
+};
+int main(){
+    vector<vector<int>>grid{
+        {0,1,1,0},
+        {1,1,0,0},
+        {0,0,1,1}
+    };
+	
+	Solution obj;
+	vector<vector<int>> ans = obj.updateMatrix(grid);
+		
+	for(auto i: ans){
+		for(auto j: i)
+			cout << j << " ";
+		cout << "\n";
+	}
+}
